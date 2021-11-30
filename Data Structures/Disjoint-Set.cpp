9211f0e45ec4fd599e9f229a774c67ed17f8fa9d
TruVortex <75190116+TruVortex@users.noreply.github.com>
2021-11-29 20:19:01 -0500

    Update Disjoint-Set.cpp

--- Data Structures/Disjoint-Set.cpp
+++ Data Structures/Disjoint-Set.cpp
@@ -1,14 +1,36 @@
 #include <stdio.h>
+#include <vector>
+#include <algorithm>
 #include <unordered_map>
 
 using namespace std;
 
+struct Edge {
+
+    int a, b, weight;
+
+    Edge() {
+        a = -1;
+        b = -1;
+        weight = -1;
+    }
+
+    Edge(int a, int b, int weight) {
+        this->a = a;
+        this->b = b;
+        this->weight = weight;
+    }
+};
+
 struct DisjointSet {
 
+    int v;
+    vector<Edge> edges;
     unordered_map<int, int> parent, rank;
 
-    DisjointSet(int n) {
-        for (int i = 0; i < n; i++) {
+    DisjointSet(int v) {
+        this->v = v;
+        for (int i = 0; i < v; i++) {
             makeSet(i);
         }
     }
@@ -16,6 +38,10 @@ struct DisjointSet {
     void makeSet(int x) {
         parent[x] = x;
         rank[x] = 0;
+    }
+
+    void addEdge(Edge edge) {
+        edges.push_back(edge);
     }
 
     int find(int x) {
@@ -36,25 +62,41 @@ struct DisjointSet {
             parent[yRt] = xRt;
         } else {
             parent[xRt] = yRt;
-            rank[yRt] = rank[xRt] + 1;
+            rank[yRt]++;
         }
+    }
+
+    static int kruskalCmp(Edge a, Edge b) {
+        return a.weight < b.weight;
+    }
+
+    vector<Edge> kruskal() {
+        vector<Edge> result;
+        sort(edges.begin(), edges.end(), kruskalCmp);
+        for (const Edge &edge : edges) {
+            if (find(edge.a) != find(edge.b)) {
+                result.push_back(edge);
+                unionn(find(edge.a), find(edge.b));
+            }
+        }
+        return result;
     }
 };
 
 int main() {
-    DisjointSet djs(5);
-    djs.unionn(0, 2);
-    djs.unionn(2, 4);
-    djs.unionn(1, 3);
-    if (djs.find(0) == djs.find(4)) {
-        printf("Yes\n");
-    } else {
-        printf("No\n");
+    DisjointSet graph(4);
+    graph.addEdge(Edge(0, 1, 10));
+    graph.addEdge(Edge(0, 2, 6));
+    graph.addEdge(Edge(0, 3, 5));
+    graph.addEdge(Edge(1, 3, 15));
+    graph.addEdge(Edge(2, 3, 4));
+    printf("MST:\n");
+    int cost = 0;
+    vector<Edge> mst = graph.kruskal();
+    for (const Edge &edge : mst) {
+        printf("%d -> %d = %d\n", edge.a, edge.b, edge.weight);
+        cost += edge.weight;
     }
-    if (djs.find(0) == djs.find(1)) {
-        printf("Yes\n");
-    } else {
-        printf("No\n");
-    }
+    printf("Cost: %d\n", cost);
     return 0;
 }
