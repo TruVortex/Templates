3fa257c809e130b801b6e7acf2289800955ee06b
TruVortex <75190116+TruVortex@users.noreply.github.com>
2021-12-03 17:07:05 -0500

    Create Dijkstra.cpp

---
+++ Search Algorithms/Dijkstra.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <queue>
+
+using namespace std;
+
+vector<int> dijkstra(vector<vector<pair<int, int>>> adj, int src) {
+    vector<int> dist(adj.size(), -1);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    dist[src] = 0;
+    pq.push({ 0, src });
+    while (!pq.empty()) {
+        int cur = pq.top().second;
+        for (const pair<int, int>& edge : adj[cur]) {
+            if (dist[edge.first] == -1 || dist[cur] + edge.second < dist[edge.first]) {
+                dist[edge.first] = dist[cur] + edge.second;
+                pq.push({ dist[edge.first], edge.first });
+            }
+        }
+        pq.pop();
+    }
+    return dist;
+}
+
+int main() {
+    vector<vector<pair<int, int>>> adj(4);
+    adj[0].push_back({ 1, 2 });
+    adj[0].push_back({ 2, 5 });
+    adj[1].push_back({ 2, 2 });
+    vector<int> dist = dijkstra(adj, 0);
+    for (int i = 0; i < 4; i++) {
+        printf("%d\n", dist[i]);
+    }
+    return 0;
+}
