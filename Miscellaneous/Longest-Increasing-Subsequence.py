677b5f12363fb8b0d4daf933dff7e167e2b7b69f
TruVortex <75190116+TruVortex@users.noreply.github.com>
2022-03-07 17:48:17 -0500

    Update Longest-Increasing-Subsequence.py
    orz

--- Miscellaneous/Longest-Increasing-Subsequence.py
+++ Miscellaneous/Longest-Increasing-Subsequence.py
@@ -7,7 +7,7 @@ def trace(dp, s, t, x, y):
   elif dp[x - 1][y] > dp[x][y - 1]: return trace(dp, s, t, x - 1, y)
   else: return trace(dp, s, t, x, y - 1)
 s, t = input().strip(), input().strip()
-dp = [[0 for i in range(len(t) + 5)] for j in range(len(s) + 5)]
+dp = [[0 for j in range(len(t) + 5)] for j in range(len(s) + 5)]
 for i in range(len(s) + 1):
   for j in range(len(t) + 1):
     if i == 0 or j == 0: dp[i][j] = 0
