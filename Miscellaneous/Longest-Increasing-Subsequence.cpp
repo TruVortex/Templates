7384868c1697656a471400f0afa29e05c179c263
uselessleaff <75755571+uselessleaff@users.noreply.github.com>
2022-03-07 17:42:12 -0500

    Update Longest-Increasing-Subsequence.cpp

--- Miscellaneous/Longest-Increasing-Subsequence.cpp
+++ Miscellaneous/Longest-Increasing-Subsequence.cpp
@@ -1,32 +1,16 @@
-#include <stdio.h>
-#include <vector>
-#include <algorithm>
-#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
-char _;
-
-using namespace std;
-
-int lis(vector<int> vect) {
-    vector<int> dp;
-    for (int x : vect) {
-        int idx = lower_bound(dp.begin(), dp.end(), x) - dp.begin();
-        if (idx == dp.size()) {
-            dp.push_back(x);
-        } else {
-            dp[idx] = x;
-        }
-    }
-    return dp.size();
-}
-
-int main() {
-    vector<int> vect;
-    int n;
-    scan(n);
-    for (int i = 0, x; i < n; i++) {
-        scan(x);
-        vect.push_back(x);
-    }
-    printf("%d", lis(vect));
-    return 0;
-}
+mport sys
+input = sys.stdin.readline
+sys.setrecursionlimit(100000)
+def trace(dp, s, t, x, y):
+  if x == 0 or y == 0: return ""
+  elif s[x - 1] == t[y - 1]: return trace(dp, s, t, x - 1, y - 1) + s[x - 1]
+  elif dp[x - 1][y] > dp[x][y - 1]: return trace(dp, s, t, x - 1, y)
+  else: return trace(dp, s, t, x, y - 1)
+s, t = input().strip(), input().strip()
+dp = [[0 for i in range(len(t) + 5)] for i in range(len(s) + 5)]
+for i in range(len(s) + 1):
+  for j in range(len(t) + 1):
+    if i == 0 or j == 0: dp[i][j] = 0
+    elif s[i - 1] == t[j - 1]: dp[i][j] = dp[i - 1][j - 1] + 1
+    elif s[i - 1] != t[j - 1]: dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+print(trace(dp, s, t, len(s), len(t)))
