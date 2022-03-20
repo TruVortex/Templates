23cf5615514a7210fa4ce109197ba386efcc7880
TruVortex <75190116+TruVortex@users.noreply.github.com>
2022-03-20 12:52:40 -0400

    Update Longest-Increasing-Subsequence.py

--- Miscellaneous/Longest-Increasing-Subsequence.py
+++ Miscellaneous/Longest-Increasing-Subsequence.py
@@ -1,20 +1,23 @@
 import sys
 import bisect
 import gc
+
 input = sys.stdin.readline
 
+
 def lis(a):
-  dp = []
-  for x in a:
-    idx = bisect.bisect_left(dp, x)
-    if idx >= len(dp):
-      dp.append(x)
-    else:
-      dp[idx] = x
-      
-  return len(dp)
+    dp = []
+    for x in a:
+        idx = bisect.bisect_left(dp, x)
+        if idx >= len(dp):
+            dp.append(x)
+        else:
+            dp[idx] = x
+
+    return len(dp)
+
 
 n = int(input())
 a = list(map(int, input().strip().split(" ")))
-gc.collect() # to pass https://dmoj.ca/problem/lis
+gc.collect()  # to pass https://dmoj.ca/problem/lis
 print(lis(a))
