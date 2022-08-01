ff0e6ef2bc3960e7aa1d3f4b2c150fb8f65e5d5c
TruVortex <75190116+TruVortex@users.noreply.github.com>
2022-08-01 11:40:09 -0400

    Update Modular-Exponentiation.cpp

--- Numbers/Modular-Exponentiation.cpp
+++ Numbers/Modular-Exponentiation.cpp
@@ -2,9 +2,9 @@ int modPow(int a, int b, int m) {
     if (b == 0) {
         return 1;
     }
-    long long temp = modPow(a, b / 2, m) % m;
+    long long temp = modPow(a, b / 2, m);
     if (b % 2 == 1) {
-        return ((temp * temp) % m) * a % m;
+        return (((temp * temp) % m) * a) % m;
     } else {
         return ((temp * temp) % m);
     }
